b33622b710e5f9ea0461c251b067e02551a57fa9
jotahtsx <joaomanoelskai@gmail.com>
2022-06-20 17:10:25 -0300

    Logando e gerando JWT Token

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -25,9 +25,24 @@ class AuthUserService{
             throw new Error("User/password incorreto") 
         }
 
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
 
-
-        return {ok: true}
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
