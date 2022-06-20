35c7e1afb5e887f232e661d36321af1a39298c48
jotahtsx <joaomanoelskai@gmail.com>
2022-06-19 22:03:48 -0300

    Finalizando cadastro de usuário

--- backend/src/controllers/user/CreateUserController.ts
+++ backend/src/controllers/user/CreateUserController.ts
@@ -8,7 +8,9 @@ class CreateUserController{
         const createUserService = new CreateUserService();
 
         const user = await createUserService.execute({
-            name, email, password
+            name, 
+            email, 
+            password
         });
 
         return res.json(user)
--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,7 @@
+import prismaClient from '../../prima'
+
+import {hash} from 'bcryptjs'
+
 interface UserRequest{
     name: string;
     email: string;
@@ -7,9 +11,36 @@ interface UserRequest{
 class CreateUserService {
     async execute({name, email, password}: UserRequest){
 
-        console.log(name);
+        if(!email){
+            throw new Error("Email incorreto")
+        }
 
-        return{name: name}
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error("O usuário já existe no sistema")
+        }
+
+        const passwordHash = await hash(password, 8)
+
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: passwordHash
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        return user;
     }
 }
 
