c313365232a32d734b2fc6a4dd0f9112421998ea
jotahtsx <joaomanoelskai@gmail.com>
2022-06-21 15:08:00 -0300

    Configurando o upload de imagem

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
-import {Router, Request, Response} from 'express';
+import {Router} from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
@@ -11,7 +12,11 @@ import {CreateProductController} from './controllers/product/CreateProductContro
 
 import { isAuthenticated } from './middleware/isAuthenticated'
 
+import uploadConfig from './config/multer'
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 router.post('/users', new CreateUserController().handle)
 
@@ -24,6 +29,6 @@ router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 // Produtos
-router.post('/product', isAuthenticated, new CreateProductController().handle)
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 export {router};
