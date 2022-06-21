8e781f619c1c7d29cc44d819e7769197c6096bb4
jotahtsx <joaomanoelskai@gmail.com>
2022-06-20 23:41:11 -0300

    dallasBreja || Listar categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController'
 import {DetailUserController} from './controllers/user/DetailUserController'
 
 import {CreateCategoryController} from './controllers/category/CreateCategoryController'
+import {ListCategoryController} from './controllers/category/ListCategoryController'
 
 import { isAuthenticated } from './middleware/isAuthenticated'
 
@@ -18,5 +19,6 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 // Categorias
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export {router};
