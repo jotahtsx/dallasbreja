941b7062ce5b6bd1faf7315fd146fcbd74ac9734
jotahtsx <joaomanoelskai@gmail.com>
2022-06-20 22:27:56 -0300

    dallBreja || Cadastro de categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,8 @@ import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import {DetailUserController} from './controllers/user/DetailUserController'
 
+import {CreateCategoryController} from './controllers/category/CreateCategoryController'
+
 import { isAuthenticated } from './middleware/isAuthenticated'
 
 const router = Router();
@@ -13,5 +15,8 @@ router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// Categorias
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export {router};
