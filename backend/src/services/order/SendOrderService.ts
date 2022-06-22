d8070363ce01885d72c59814862ca3d6842868de
jotahtsx <joaomanoelskai@gmail.com>
2022-06-22 12:49:32 -0300

    dallasBreja || Criando service e controller de enviar pedido

---
+++ backend/src/services/order/SendOrderService.ts
@@ -0,0 +1,23 @@
+import prismaClient from "../../prisma";
+
+interface OrderReQuest{
+    order_id: string;
+}
+
+class SendOrderService{
+    async execute({order_id}: OrderReQuest){
+        const order = await prismaClient.order.update({
+            where: {
+                id: order_id
+            },
+            data: {
+                draft: false
+            }
+        })
+
+        return order;
+
+    }
+}
+
+export { SendOrderService }
