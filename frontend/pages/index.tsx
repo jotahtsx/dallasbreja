6cab5d807552f5719b6154af39394a68c702d9b9
jotahtsx <joaomanoelskai@gmail.com>
2022-06-22 20:35:39 -0300

    dallasBreja || Setando Typescript e editorconfig

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+type Props = {
+  title: string;
+};
+
+export default function Home({title = "Dallas Bar"}: Props) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <main>
+      </main>
+    </div>
+  );
+}
