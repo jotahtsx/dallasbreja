83fc69b8b6db9252326d7981020c827d447a76b4
jotahtsx <joaomanoelskai@gmail.com>
2022-06-27 15:35:09 -0300

    dallasBreja || Efetuando login através do frontend

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,9 +1,15 @@
 import { createContext, ReactNode, useState } from 'react'
 
+import { api } from 'services/apiClient'
+
+import { destroyCookie, setCookie } from 'nookies'
+import Router from 'next/router'
+
 type AuthContextData = {
   user?: UserProps
   isAuthenticated: boolean
   signIn: (credentials: SignInProps) => Promise<void>
+  signOut: () => void
 }
 
 type UserProps = {
@@ -23,17 +29,51 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut() {
+  try {
+    destroyCookie(undefined, '@dallasfridayauth.token')
+    Router.push('/')
+  } catch {
+    console.log('Erro ao sair')
+  }
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user] = useState<UserProps>()
+  const [user, setUser] = useState<UserProps>()
   const isAuthenticated = !!user
 
   async function signIn({ email, password }: SignInProps) {
-    console.log('Dados para logar ', email)
-    console.log('senha ', password)
+    try {
+      const response = await api.post('/session', {
+        email,
+        password,
+      })
+      // console.log(response.data)
+
+      const { id, name, token } = response.data
+
+      setCookie(undefined, '@dallasfridayauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: '/', // Caminhos(Paths) que terão acesso ao cookie gerado
+      })
+
+      setUser({
+        id,
+        name,
+        email,
+      })
+
+      //Passa para as próximas requisições o token que foi gerado
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+      Router.push('/dashboard')
+    } catch (err) {
+      console.log('Erro ao acessar ', err)
+    }
   }
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   )
