a182f6a79337b66553b817429d850906cceaf844
jotahtsx <joaomanoelskai@gmail.com>
2022-06-28 16:56:34 -0300

    dallasBreja || Finalizando rotas privadas

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,3 +1,20 @@
+import Head from 'next/head'
+
+import { canSSRAuth } from 'utils/canSSRAuth'
+
 export default function Dashboard() {
-  return <div>dashboard</div>
+  return (
+    <>
+      <Head>
+        <title>Dallas Breja - Visão Geral</title>
+      </Head>
+      <h1>dashboard</h1>
+    </>
+  )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  return {
+    props: {},
+  }
+})
