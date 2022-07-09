0e7d11d136f6351d56f06ac252408f260b18fd0b
jotahtsx <joaomanoelskai@gmail.com>
2022-07-09 09:59:59 -0300

    dallasBreja || Criando tela de novo pedido

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,5 +1,13 @@
-import React, {useContext} from "react"
-import {View, Text, Button} from "react-native"
+import React, {useContext, useState} from "react"
+import 
+    {
+        View, 
+        Text, 
+        SafeAreaView, 
+        TouchableOpacity, 
+        TextInput,
+        StyleSheet
+    } from "react-native"
 
 import {AuthContext} from '../../context/AuthContext'
 
@@ -7,9 +15,61 @@ export default function Dashboard() {
     const {signOut} = useContext(AuthContext)
 
     return (
-        <View>
-            <Text>Visão Geral</Text>
-            <Button title="Sair" onPress={signOut}></Button>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Novo pedido</Text>
+            <TextInput 
+                placeholder="Número da mesa"
+                placeholderTextColor="#576574"
+                style={styles.input}
+                keyboardType="numeric"
+            />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Abrir mesa</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 15,
+        backgroundColor: '#181820'
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 24
+    },
+    input: {
+        width: '90%',
+        height: 50,
+        textAlign: 'center',
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: '#fff',
+        marginBottom: 15,
+        borderStyle: 'solid',
+        borderColor: '#c8d6e5',
+        borderWidth: 2,
+        borderRadius: 10,
+        fontSize: 16
+    },
+    button: {
+        width: '90%',
+        height: 60,
+        backgroundColor: '#ECAE04',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        color: '#000'
+    }
+})
