b3dea1a950f5526c5abd716f24365491a3d58ba7
jotahtsx <joaomanoelskai@gmail.com>
2022-07-09 08:24:58 -0300

    dallasBreja || pega os dados salvos do user e verifica se a informação chegou

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -1,8 +1,12 @@
-import React, {useState, createContext, ReactNode} from 'react'
+import React, {useState, createContext, ReactNode, useEffect} from 'react'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import {api} from '../services/api'
 
 type AuthContextData = {
     user: UserProps
-    isAthenticated: boolean,
+    isAuthenticated: boolean,
     signIn: (credentials: SignInProps) => Promise<void>
 }
 
@@ -33,15 +37,71 @@ export function AuthProvider({children}: AuthProviderProps){
         token: ''
     })
 
-    const isAthenticated = !!user.name
+    const [loadingAuth, setLoadingAuth] = useState(false)
+
+    const isAuthenticated = !!user.name
+
+    useEffect(() => {
+
+        async function getUser(){
+            const userInfo = await AsyncStorage.getItem('@dallasbreja')
+            let hasUser: UserProps = JSON.parse(userInfo || '{}')
+
+            if(Object.keys(hasUser).length > 0){
+                api.defaults.headers.common['Authorization'] = `Bearer ${hasUser.token}`
+
+                setUser({
+                    id: hasUser.id,
+                    name: hasUser.name,
+                    email: hasUser.email,
+                    token: hasUser.token
+                })
+
+            }
+        }
+
+        getUser()
+
+    }, [])
 
     async function signIn({email, password}: SignInProps){
-        console.log(email)
-        console.log(password)
+        setLoadingAuth(true)
+
+        try{
+            const response = await api.post('/session', {
+                email,
+                password
+            })
+
+            // console.log(response.data)
+
+            const {id, name, token} = response.data
+            
+            const data = {
+                ...response.data
+            }
+
+            await AsyncStorage.setItem('@dallasbreja', JSON.stringify(data))
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+            
+            setUser({
+                id,
+                name,
+                email,
+                token
+            })
+
+            setLoadingAuth(false)
+
+        }catch(err){
+            console.log('Erro ao acessar', err)
+            setLoadingAuth(false)
+        }
     }
 
     return(
-        <AuthContext.Provider value={{user, isAthenticated, signIn}}>
+        <AuthContext.Provider value={{user, isAuthenticated, signIn}}>
             {children}
         </AuthContext.Provider>
     )
